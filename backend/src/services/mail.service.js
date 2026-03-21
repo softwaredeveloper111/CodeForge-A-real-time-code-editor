7032ff5548c65fe7c302b9ea209ea347617dc2e6
Sourav Giri <computerscienceengineer1@gmail.com>
2026-03-21 22:22:55 +0530

    trying to solve smtp server 1

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -3,6 +3,10 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,          
+  secure: false, 
+
   auth: {
     type: 'OAuth2',
     user: process.env.EMAIL_USER,
@@ -10,6 +14,9 @@ const transporter = nodemailer.createTransport({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
   },
+
+  family: 4,
+
 });
 
 
